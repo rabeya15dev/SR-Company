029d5e2aedd6b5afcf27bf2134ec564f92f42812
rabeya <rabeya1012000@gmail.com>
2023-05-23 01:32:59 +0600

    2nd

--- index .php
+++ index .php
@@ -189,24 +189,24 @@
     <div class="w-50 m-auto">
       <form action="userinfo.php" method="post">
        <div class="form-group">
-         <label>Name</label>
+         <label> <b>Name</b> </label>
          <input type="text" name="user" autocomplete="off" class="form-control">
        </div> 
         <div class="form-group">
-         <label>Email </label>
+         <label><b>Email</b>  </label>
          <input type="text" name="user" autocomplete="off" class="form-control">
        </div> 
 
        <div class="form-group">
-         <label>Mobile</label>
+         <label> <b>Mobile</b> </label>
          <input type="text" name="user" autocomplete="off" class="form-control">
        </div> 
        <div class="form-group">
-         <label>Comment</label>
+         <label> <b>Comment</b> </label>
          <textarea class="form-control" name ="comment" >
          </textarea>
        </div> 
-       <button type="submit" class="btn btn-primary">Submit</button>
+       <button type="submit" class="btn btn-primary"><b> Submit</b></button>
       </form>
    
     </div>
